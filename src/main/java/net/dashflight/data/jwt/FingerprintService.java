25a9316fd43cea5dbd526afd7d2910249a78451f
jbeck18 <j.beck18@me.com>
2020-03-19 13:25:28 -0400

    Added jwt operations

---
+++ src/main/java/net/dashflight/data/jwt/FingerprintService.java
@@ -0,0 +1,53 @@
+package net.dashflight.data.jwt;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import javax.xml.bind.DatatypeConverter;
+import net.dashflight.data.random.LavaRandom;
+
+/**
+ * Utility for generating token fingerprints to strengthen security of JWTs
+ */
+public class FingerprintService {
+
+    private static final int FINGERPRINT_LENGTH = 64;
+
+    private SecureRandom secureRandom;
+    private MessageDigest digest;
+
+
+    public FingerprintService() {
+        this.secureRandom = new LavaRandom();
+
+        try {
+            this.digest = MessageDigest.getInstance("SHA-256");
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Generates a random fingerprint of length FINGERPRINT_LENGTH
+     */
+    public String generateRandomFingerprint() {
+        byte[] randomBytes = new byte[FINGERPRINT_LENGTH];
+        secureRandom.nextBytes(randomBytes);
+
+        return DatatypeConverter.printHexBinary(randomBytes);
+    }
+
+    /**
+     * Hashes a fingerprint with SHA-256
+     */
+    public String hashFingerprint(String fgp) {
+        if (fgp == null) {
+            return null;
+        }
+
+        byte[] fingerprintDigest = digest.digest(fgp.getBytes(StandardCharsets.UTF_8));
+        return DatatypeConverter.printHexBinary(fingerprintDigest);
+    }
+
+}
---
+++ src/main/java/net/dashflight/data/jwt/JwtOperator.java
@@ -0,0 +1,23 @@
+package net.dashflight.data.jwt;
+
+import net.dashflight.data.config.Configurable;
+import net.dashflight.data.keys.DefaultRSAKeyManager;
+import net.dashflight.data.keys.RSAKeyManagerFactory;
+
+/**
+ * Base class for services handling Jwt manipulation.
+ * Stores some common fields necessary for Jwt creation/verification.
+ */
+public abstract class JwtOperator implements Configurable {
+
+    protected static final String ISSUER = "https://api.dashflight.net";
+
+    // protected final TokenCipher tokenCipher = new TokenCipher();
+    protected final FingerprintService fgpService = new FingerprintService();
+
+    protected DefaultRSAKeyManager keyManager = RSAKeyManagerFactory.withDefaults();
+
+    public JwtOperator() {
+        registerWith("jwt-utils");
+    }
+}
