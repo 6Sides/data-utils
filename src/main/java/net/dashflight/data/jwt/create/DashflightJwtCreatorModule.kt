ae6423716a35495cf1d9bb6582e69a980889362d
jbeck18 <j.beck18@me.com>
2020-05-04 09:35:35 -0400

    Updated random module

--- src/main/java/net/dashflight/data/jwt/create/DashflightJwtCreatorModule.kt
+++ src/main/java/net/dashflight/data/jwt/create/DashflightJwtCreatorModule.kt
@@ -3,6 +3,7 @@ package net.dashflight.data.jwt.create
 import com.google.inject.AbstractModule
 import net.dashflight.data.jwt.create.request.DashflightCreateJwtRequestProviderModule
 import net.dashflight.data.random.LavaRandom
+import java.security.SecureRandom
 import java.util.*
 
 class DashflightJwtCreatorModule : AbstractModule() {
@@ -11,6 +12,6 @@ class DashflightJwtCreatorModule : AbstractModule() {
         install(DashflightCreateJwtRequestProviderModule())
 
         bind(JwtCreator::class.java)
-        bind(Random::class.java).to(LavaRandom::class.java)
+        bind(Random::class.java).to(SecureRandom::class.java)
     }
 }
