822ef089bef953e03a56be6c2047f2409bda1f7d
jbeck18 <j.beck18@me.com>
2020-05-15 14:06:16 -0400

    Added logging

--- src/main/java/net/dashflight/data/caching/RefreshAheadCachedFetcher.kt
+++ src/main/java/net/dashflight/data/caching/RefreshAheadCachedFetcher.kt
@@ -1,6 +1,7 @@
 package net.dashflight.data.caching
 
 import com.google.inject.Inject
+import net.dashflight.data.logging.logger
 import net.dashflight.data.redis.RedisClient
 import redis.clients.jedis.JedisPool
 import java.util.concurrent.Executors
@@ -9,6 +10,13 @@ import java.util.concurrent.Executors
  * Implementation of the refresh-ahead caching strategy.
  */
 abstract class RefreshAheadCachedFetcher<K, V> @Inject protected constructor(redisClient: RedisClient): CachedFetcher<K, V?>(redisClient) {
+
+    companion object {
+        private val LOG by logger()
+        // Should be in (0, 1). The higher the value, the longer the value waits to be refreshed.
+        private const val REFRESH_AHEAD_FACTOR = 0.5f
+        private val threadPool = Executors.newCachedThreadPool()
+    }
 
     private val redisPool: JedisPool = redisClient.pool
 
@@ -37,16 +45,13 @@ abstract class RefreshAheadCachedFetcher<K, V> @Inject protected constructor(red
 
         // Refetch result and cache it
         if (result == null) {
+            LOG.debug { "Cache miss for key $input. Recomputing value..." }
             result = calculateResult(input)
             cacheResult(input, result)
+        } else {
+            LOG.debug { "Cache hit for key $input" }
         }
 
         return result
-    }
-
-    companion object {
-        // Should be in (0, 1). The higher the value, the longer the value waits to be refreshed.
-        private const val REFRESH_AHEAD_FACTOR = 0.5f
-        private val threadPool = Executors.newCachedThreadPool()
     }
 }
