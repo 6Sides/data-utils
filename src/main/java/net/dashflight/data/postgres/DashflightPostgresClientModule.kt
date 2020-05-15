6b6bf5731eb14c85d1189980ec81dd876d467f3f
jbeck18 <j.beck18@me.com>
2020-05-15 11:36:24 -0400

    Added new runtime environment

--- src/main/java/net/dashflight/data/postgres/DashflightPostgresClientModule.kt
+++ src/main/java/net/dashflight/data/postgres/DashflightPostgresClientModule.kt
@@ -5,6 +5,6 @@ import com.google.inject.AbstractModule
 class DashflightPostgresClientModule constructor(private val applicationName: String? = null): AbstractModule() {
 
     override fun configure() {
-        bind(PostgresConnectionOptionProvider::class.java).toInstance(DashflightPostgresConnectionOptionProvider(applicationName))
+        bind(PostgresConnectionOptionProvider::class.java).toInstance(DashflightPostgresConnectionOptionProvider(applicationName ?: "java-postgres"))
     }
 }
--- src/main/java/net/dashflight/data/postgres/DashflightPostgresConnectionOptionProvider.kt
+++ src/main/java/net/dashflight/data/postgres/DashflightPostgresConnectionOptionProvider.kt
@@ -1,33 +1,28 @@
 package net.dashflight.data.postgres
 
-import net.dashflight.data.config.Configurable
 import net.dashflight.data.config.S3ConfigurationDelegate
 
 /**
  * Pulls the required postgres connection options from s3
  */
-class DashflightPostgresConnectionOptionProvider @JvmOverloads constructor(applicationName: String? = null) : PostgresConnectionOptionProvider, Configurable {
+class DashflightPostgresConnectionOptionProvider @JvmOverloads constructor(applicationName: String = DEFAULT_APP_NAME) : PostgresConnectionOptionProvider {
 
-    init {
-        registerWith(applicationName ?: DEFAULT_APP_NAME)
-    }
+    private val host: String? by S3ConfigurationDelegate(applicationName, "pg_host")
 
-    private val host: String? by S3ConfigurationDelegate(DEFAULT_APP_NAME, "pg_host")
+    private val port: Int by S3ConfigurationDelegate(applicationName, "pg_port")
 
-    private val port: Int by S3ConfigurationDelegate(DEFAULT_APP_NAME, "pg_port")
+    private val dbname: String? by S3ConfigurationDelegate(applicationName, "pg_dbname")
 
-    private val dbname: String? by S3ConfigurationDelegate(DEFAULT_APP_NAME, "pg_dbname")
+    private val username: String? by S3ConfigurationDelegate(applicationName, "pg_username")
 
-    private val username: String? by S3ConfigurationDelegate(DEFAULT_APP_NAME, "pg_username")
+    private val password: String? by S3ConfigurationDelegate(applicationName, "pg_password")
 
-    private val password: String? by S3ConfigurationDelegate(DEFAULT_APP_NAME, "pg_password")
+    private val _applicationName: String? by S3ConfigurationDelegate(applicationName, "application_name")
 
-    private val applicationName: String? by S3ConfigurationDelegate(DEFAULT_APP_NAME, "application_name")
-
-    private val maxPoolSize: Int by S3ConfigurationDelegate(DEFAULT_APP_NAME, "max_pool_size")
+    private val maxPoolSize: Int by S3ConfigurationDelegate(applicationName, "max_pool_size")
 
     override fun get(): PostgresConnectionOptions {
-        return PostgresConnectionOptions(host!!, port, dbname, username, password, applicationName ?: "Dashflight Java App", maxPoolSize)
+        return PostgresConnectionOptions(host!!, port, dbname, username, password, _applicationName ?: "Dashflight Java App", maxPoolSize)
     }
 
     companion object {
