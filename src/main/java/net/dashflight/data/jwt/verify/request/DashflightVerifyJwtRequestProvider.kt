bb167b719c82bc471a79e03b5564cc92911f3d9d
jbeck18 <j.beck18@me.com>
2020-07-06 11:02:17 -0400

    Refactored jwt creation/verification modules to use Hydro

--- src/main/java/net/dashflight/data/jwt/verify/request/DashflightVerifyJwtRequestProvider.kt
+++ src/main/java/net/dashflight/data/jwt/verify/request/DashflightVerifyJwtRequestProvider.kt
@@ -1,6 +1,7 @@
 package net.dashflight.data.jwt.verify.request
 
 import com.google.inject.Inject
+import hydro.engine.Hydro.hydrate
 import net.dashflight.data.config.ConfigValue
 import net.dashflight.data.config.Configurable
 import net.dashflight.data.jwt.FingerprintService
@@ -9,11 +10,9 @@ import net.dashflight.data.keys.RSAKeyPairProvider
 class DashflightVerifyJwtRequestProvider @Inject constructor(
         private val fingerprintService: FingerprintService,
         private val keyManager: RSAKeyPairProvider
-) : VerifyJwtRequestProvider, Configurable {
+) : VerifyJwtRequestProvider {
 
-    init {
-        registerWith("jwt-utils")
-    }
+    private val ISSUER: String by hydrate("issuer")
 
     override fun create(token: String, fingerprint: String): JwtVerificationRequirements {
         return JwtVerificationRequirements(
@@ -23,10 +22,5 @@ class DashflightVerifyJwtRequestProvider @Inject constructor(
                 fingerprintHash = fingerprintService.hashFingerprint(fingerprint),
                 publicKey = keyManager.publicKey
         )
-    }
-
-    companion object {
-        @ConfigValue("issuer")
-        private lateinit var ISSUER: String
     }
 }
